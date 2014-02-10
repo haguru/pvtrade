c02cba39e712831ac5d9f68174a6efde18949378
haguru <marc.p.fuller@gmail.com>
2014-02-09 21:58:16 -0600

    pv Trade

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pvtrade::Application.config.secret_key_base = '5e76886096ffcd8df426c5a5b632c3fdf13f6047a6a787dbb0e1d48a1b21a1714e5979329e9e893dcb4c26da7ea0a52ac0b83477eb391fd6a4ce9165deb3946c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Pvtrade::Application.config.session_store :cookie_store, key: '_pvtrade_session'
